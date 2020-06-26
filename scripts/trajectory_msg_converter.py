5d5391f811c91c2bf3e5bb9be7baa0df6c743818
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2020-06-27 03:48:26 +0400

    Add node for converting traj msg

---
+++ scripts/trajectory_msg_converter.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+"""@trajectory_msg_converter.py
+This node converts Fast-Planner reference trajectory message to MultiDOFJointTrajectory which is accepted by geometric_controller
+Authors: Mohamed Abdelkader
+"""
+
+# Imports
+import rospy
+from trajectory_msgs.msg import MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint # for geometric_controller
+from quadrotor_msgs.msg import PositionCommand # for Fast-Planner
+from geometry_msgs.msg import Transform, Twist
+from tf.transformations import quaternion_from_euler
+
+class MessageConverter:
+    def __init__(self):
+        rospy.init_node('trajectory_msg_converter')
+
+        fast_planner_traj_topic = rospy.get_param('~fast_planner_traj_topic', 'planning/pos_cmd')
+        traj_pub_topic = rospy.get_param('~traj_pub_topic', 'command/trajectory')
+
+        # Publisher for geometric_controller
+        self.traj_pub = rospy.Publisher(traj_pub_topic, MultiDOFJointTrajectory, queue_size=1)
+
+        # Subscriber for Fast-Planner reference trajectory
+        rospy.Subscriber(fast_planner_traj_topic, PositionCommand, self.fastPlannerTrajCallback, tcp_nodelay=True)
+
+        rospy.spin()
+
+    def fastPlannerTrajCallback(self, msg):
+        # position and yaw
+        pose = Transform()
+        pose.translation.x = msg.position.x
+        pose.translation.y = msg.position.y
+        pose.translation.z = msg.position.z
+        q = quaternion_from_euler(0, 0, msg.yaw) # RPY
+        pose.rotation.x = q[0]
+        pose.rotation.y = q[1]
+        pose.rotation.z = q[2]
+        pose.rotation.w = q[3]
+
+        # velocity
+        vel = Twist()
+        vel.linear = msg.velocity
+        # TODO: set vel.angular to msg.yaw_dot
+
+        # acceleration
+        acc = Twist()
+        acc.linear = msg.acceleration
+
+        traj_point = MultiDOFJointTrajectoryPoint()
+        traj_point.transforms.append(pose)
+        traj_point.velocities.append(vel)
+        traj_point.accelerations.append(acc)
+
+        traj_msg = MultiDOFJointTrajectory()
+
+        traj_msg.header = msg.header
+        traj_msg.points.append(traj_point)
+        self.traj_pub.publish(traj_msg)
+
+if __name__ == '__main__':
+    obj = MessageConverter()
